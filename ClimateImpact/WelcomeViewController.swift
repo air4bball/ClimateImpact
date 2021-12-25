ad2e8b4626b44f4ddb54e7659210cf10199cb58c
SrikarP <air4bball@gmail.com>
2021-12-24 16:12:12 -0800

    worked on welcome screen

--- ClimateImpact/WelcomeViewController.swift
+++ ClimateImpact/WelcomeViewController.swift
@@ -11,7 +11,7 @@ class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
     
