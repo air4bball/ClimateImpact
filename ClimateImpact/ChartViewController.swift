b87b7a0136d82e2c2890249d2ee79a22218fc333
Pranav Kumar Soma <pranavsoma11@gmail.com>
2021-12-25 00:21:56 -0800

    DATA COMPLETED!!! OMG YASSS SLAY!!!!!! Serious Commit: Graph has been created.

--- ClimateImpact/ChartViewController.swift
+++ ClimateImpact/ChartViewController.swift
@@ -6,12 +6,37 @@
 //
 
 import UIKit
+import Charts
 
 class ChartViewController: UIViewController {
-
+    @IBOutlet weak var Chart: LineChartView!
+    
+    
+    var numbers = [Double](); // data
+    var LineChartEntry = [ChartDataEntry]();
+    
+    func updateGraph(){
+        for i in 0..<numbers.count{
+            let value = ChartDataEntry(x:Double(i),y:numbers[i])
+            LineChartEntry.append(value)
+        }
+        
+        let line = LineChartDataSet(entries: LineChartEntry, label: "Number")
+        line.colors = [NSUIColor.blue]
+        
+        let data = LineChartData()
+        data.addDataSet(line)
+        
+        Chart.data = data
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateGraph()
     }
     
     
