56c2aefcc2046d2ea132ec2b3e30731b17ab1882
SrikarP <air4bball@gmail.com>
2021-12-25 18:20:33 -0800

    added arrays for counties and risk factors

--- ClimateImpact/RiskIndexViewController.swift
+++ ClimateImpact/RiskIndexViewController.swift
@@ -11,6 +11,9 @@ class RiskIndexViewController: UIViewController {
     
     @IBOutlet weak var indexBar: UIProgressView!
     
+    var riskIndex = [[5.3, 4, 4], [0, 2, 6], [2, 2, 6], [7.4, 0, 6], [3.8, 2, 4], [0, 1, 8], [4.4, 4, 4], [0, 0, 4], [5.5, 2, 4], [6.9, 2, 4], [0.4, 0, 6], [4.2, 0, 4], [0, 1, 2], [1.8, 0, 4], [6.7, 0, 2], [0, 2, 2,], [6.1, 1, 6], [5.4, 0, 4], [6.2, 0, 4], [5.4, 2, 6], [0, 1, 8], [5.3, 3, 4], [4.6, 0, 4], [2.7, 4, 6], [4.9, 0, 6], [1.2, 2, 4], [6.1, 2, 4], [4.2, 2, 6], [3.1, 0, 4], [1.2, 0, 4], [2.9, 1, 4], [1.5, 0, 6], [10, 1, 4], [1.5, 3, 6], [3.4, 3, 4], [5.4, 0, 4], [8.4, 0, 4], [0, 4, 4,], [0.4, 4, 6], [7.3, 1, 4], [0, 3, 6], [4.7, 0, 4], [5.6, 4, 4], [0, 3, 4], [7.3, 0, 6], [0, 0, 6], [6.9, 0, 6], [3.3, 4, 4], [3.1, 2, 6], [2, 4, 4], [0, 2, 8], [6.5, 0, 6], [3.6, 0, 6], [5.3, 0, 4], [3.6, 2, 2], [4.8, 0, 4], [1.8, 2, 6], [2.3, 0, 4]]
+    var counties = ["ALAMEDA", "ALPINE", "AMADOR", "BUTTE", "CALAVERAS", "COLUSA", "CONTRA COSTA", "DEL NORTE", "EL DORADO", "FRESNO", "GLENN", "HUMBOLDT", "IMPERIAL", "INYO", "KERN", "KINGS", "LAKE", "LASSEN", "LOS ANGELES", "MADERA", "MARIN", "MARIPOSA", "MENDOCINO", "MERCED", "MODOC", "MONO", "MONTEREY", "NAPA", "NEVADA", "ORANGE", "PLACER", "PLUMAS", "RIVERSIDE", "SACRAMENTO", "SAN BENITO", "SAN BERNARDINO", "SAN DIEGO", "SAN FRANCISCO", "SAN JOAQUIN", "SAN LUIS OBISPO", "SAN MATEO", "SANTA BARBARA", "SANTA CLARA", "SANTA CRUZ", "SHASTA", "SIERRA", "SISKIYOU", "SOLANO", "SONOMA", "STANISLAUS", "SUTTER", "TEHAMA", "TRINITY", "TULARE", "TUOLUMNE", "VENTURA", "YOLO", "YUBA"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
